2beb0b352707e57989433788c19fde53c6b08299
ihor-vip <vip20140902@gmail.com>
2023-05-25 17:39:26 +0300

    get exercise data for Exercises.js, and make it UI

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,19 @@
-export default function Exercises() {
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+   console.log(exercises);
+
     return (
-        <div>
-            Exercises
-        </div>
-    )
-}
+        <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+            <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
+            <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+                {exercises.map((exercise, idx) => (
+                    <p>{exercise.name}</p>
+                ))}
+            </Stack>
+        </Box>
+    );
+};
+
+export default Exercises;
