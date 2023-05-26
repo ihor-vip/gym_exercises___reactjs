9e9ce4687e3f7d9fe6e6209ff75cfb45bb5b6724
ihor-vip <vip20140902@gmail.com>
2023-05-26 09:19:39 +0300

    create exercise card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import ExerciseCard from "./ExerciseCard";
+
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-   console.log(exercises);
+    console.log(exercises)
 
     return (
         <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
             <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
             <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
                 {exercises.map((exercise, idx) => (
-                    <p>{exercise.name}</p>
+                    <ExerciseCard key={idx} exercise={exercise} />
                 ))}
             </Stack>
         </Box>
